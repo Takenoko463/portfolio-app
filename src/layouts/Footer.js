06c4e05022937464da4c39b028599d74689e7c43
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-05-22 01:40:54 +0900

    header main footerの基本要素を表示

---
+++ src/layouts/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024　濵﨑武史</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/layouts/Header.js
@@ -0,0 +1,26 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#skill">Skill</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
