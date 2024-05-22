196d2a6dd5bf3c4b136b6e7893b1266a56e0e4e5
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-05-22 18:26:58 +0900

    自己紹介を追加

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./layouts/Header";
 import Footer from "./layouts/Footer";
 import Projects from "./components/Projects";
+import About from "./components/About";
 import Container from "react-bootstrap/Container";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <main>
         <Container fluid="md">
+          <About />
           <Projects />
         </Container>
       </main>
